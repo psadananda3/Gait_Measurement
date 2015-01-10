a3e02d153b8730931a1bf6be0c5babfece127292
psadananda3 <priyanka.sadananda@gmail.com>
2015-01-10 17:48:03 -0500

    Update plotcode.py

--- plotcode.py
+++ plotcode.py
@@ -5,12 +5,12 @@ import pylab
 data=np.loadtxt("sensor.txt")
 
 sonic1=data[:,0]
-prox1=data[:,1]
-pir1=data[:,2]
+pir1=data[:,1]
+prox1=data[:,2]
 
 sonic2=data[:,3]
-prox2=range(1,len(data),1);
-pir2=range(1,len(data),1);
+pir2=data[:,4]
+prox2=data[:,5]
 
 
 fig = plt.figure()
@@ -26,31 +26,30 @@ def plotsensor(ax,x):
 #ULTRASONIC1
 ax1 = fig.add_subplot(321)
 plotsensor(ax1,sonic1)  
-ax1.legend(['ultrasonic1'],prop={'size':11})  
-
-#PROXIMITY1
-ax1 = fig.add_subplot(322)  
-plotsensor(ax1,prox1) 
-ax1.legend(['Proximity1'],prop={'size':11})   
+ax1.legend(['ultrasonic1'],prop={'size':11}) 
+#ULTRASONIC2
+ax1 = fig.add_subplot(322) 
+plotsensor(ax1,sonic2)  
+ax1.legend(['ultrasonic2'],prop={'size':11}) 
 
 #PIR1
 ax1 = fig.add_subplot(323)  
 plotsensor(ax1,pir1) 
 ax1.legend(['PIR1'],prop={'size':11})    
 
-#ULTRASONIC2
-ax1 = fig.add_subplot(324) 
-plotsensor(ax1,sonic2)  
-ax1.legend(['ultrasonic2'],prop={'size':11})  
+#PIR2
+ax1 = fig.add_subplot(324)    
+plotsensor(ax1,pir2)   
+ax1.legend(['PIR2'],prop={'size':11})   
+
 
 #PROXIMITY1
 ax1 = fig.add_subplot(325)  
+plotsensor(ax1,prox1) 
+ax1.legend(['Proximity1'],prop={'size':11})   
+#PROXIMITY2
+ax1 = fig.add_subplot(326)  
 plotsensor(ax1,prox2)  
-ax1.legend(['Proximity2'],prop={'size':11})   
+ax1.legend(['Proximity2'],prop={'size':11}) 
 
-#PIR1
-ax1 = fig.add_subplot(326)    
-plotsensor(ax1,pir2)   
-ax1.legend(['PIR2'],prop={'size':11})   
 plt.show()
-
